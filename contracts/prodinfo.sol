fb245044bccf452307ac4dbe181ccbc7aec7a78c
Shivendra Singh <92018084+Shivendra-Singh-Code@users.noreply.github.com>
2023-01-01 22:30:51 +0530

    Add files via upload

---
+++ contracts/prodinfo.sol
@@ -0,0 +1,15 @@
+pragama solidity > =0.4.0 <0.8.17;
+contract prodinfo{
+    struct prod{
+        string proid;
+        string prodname;
+        string currentState;
+        string timestamp;
+        string souadd; //source address
+        string desadd; //dersination address
+
+    }
+    function get_prodata() public view return(string memory){
+        
+    }
+}
