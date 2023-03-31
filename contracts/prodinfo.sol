0e68d2a0bff9534f641cb1bf3754ebc62aeb59d8
Shivendra Singh <92018084+Shivendra-Singh-Code@users.noreply.github.com>
2023-03-31 17:56:22 +0530

    Update prodinfo.sol
    States add

--- contracts/prodinfo.sol
+++ contracts/prodinfo.sol
@@ -1,108 +1,91 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.4.0 <0.9.0;
-contract prodinfo{
-    address public owner;
-    constructor() public {
-        owner=msg.sender;
 
-    }
-    modifier onlyOwner{
-        require(msg.sender==owner);
-        _;
-    }
-    struct prod{
-        uint blockno; 
-         //string proid;
-        string prodname;
+
+pragma solidity >=0.4.0 <0.9.0;
+
+contract prodinfo{
+    
+    struct States{
+        //uint blockno; 
+
         string currentState;
+
         uint time_stamp;
+
         string souadd; //source address
+
         string desadd; //dersination address
+
         address roleAddress;
+
         string role;
     }
 
-   // co
-    mapping( string => prod) public prodata;
-    /*function create_prod(//uint _blockno, 
-                        string memory _proid,
-                        string memory _prodname,
+    struct prod{
+
+        uint256 proid;
+
+        string proname;
+
+        uint256 totalStates;
+
+        mapping(uint256=> States) positions; 
+
+       
+
+    }
+
+    mapping( uint256 => prod) public prodata;
+    uint256 item=0;
+
+    function create_prod(
+                            uint256  _proid,
+
+                            string memory _proname
+
+                        ) public returns ( bool) {
+                                //uint blockno=1;
+                                prod memory _prod=prod({proid: _proid, proname: _proname, totalStates: 0});
+                                
+                                prodata[_proid]=_prod;
+                                return true;
+
+                                                              
+                        }
+    function updatestate(   
+                        uint _proid,
+
                         string memory _currentState,
+
                         //uint _timestamp,
+
                         string memory _souadd,
+
                         string memory _desadd,
-                        address _roleAddress,
-                        string memory _role)public onlyOwner{
-                                            uint blockno=0;
-                                            //blockno=string(_blockno++);
-                                            blockno++;
-                                            uint timestamp;
-                                            timestamp=now;
-                                            prodata[_proid]=prod(blockno,
-                                                                _prodname,
-                                                                _currentState,
-                                                                timestamp,
-                                                                _souadd,
-                                                                _desadd,
-                                                                _roleAddress,
-                                                                _role);
-                                           
-                                           //prodata[_proid]._prodname=_prodmane;
 
-                                        }*/
-    
-    
-    function create_prod(//uint _blockno, 
-                        string memory _proid,
-                        string memory _prodname,
-                        string memory _currentState,
-                        //uint _timestamp,
-                        string memory _souadd,
-                        string memory _desadd,
-                        address _roleAddress,
-                        string memory _role)public onlyOwner{
-                                            uint blockno=1;
-                                            //blockno=string(_blockno++);
-                                            prod memory _prod;
-                                            blockno++;
-                                            //uint _timestamp;
-                                            //_timestamp=block.timestamp;
-                                            _prod.blockno=blockno;
-                                            //_prod.proid=_proid;
-                                            _prod.prodname=_prodname;
-                                            _prod.currentState=_currentState;
-                                            _prod.time_stamp=block.timestamp;
-                                            _prod.souadd=_souadd;
-                                            _prod.desadd=_desadd;
-                                            _prod.roleAddress=_roleAddress;
-                                            _prod.role=_role;
-                                            prodata[_proid]=_prod;
-                                           
-                                           
+                        //address _roleAddress,
 
-                                        }
-    
-    function get_data(string memory proid) public view returns (uint) {
-        return( prodata[proid].blockno);
-    }                                    
+                        string memory _role ) public returns (string memory){
 
-    function update_data((//uint _blockno, 
-                        string memory _proid,
-                        string memory _prodname,
-                        string memory _currentState,
-                        //uint _timestamp,
-                        string memory _souadd,
-                        string memory _desadd,
-                        address _roleAddress,
-                        string memory _role)public onlyOwner{
-                        uint blockup;
-                        blockup=prodata[proid].blockno;
-                        blockup++;
+                            
 
-
+                            States memory newState = States({ currentState: _currentState,
+                                                            time_stamp: block.timestamp ,
+                                                            souadd: _souadd, 
+                                                            desadd: _desadd,
+                                                            roleAddress: msg.sender,
+                                                            role: _role   });
+                            
+                            prodata[_proid].positions[ prodata[_proid].totalStates ]=newState;
+        
+                            prodata[_proid].totalStates = prodata[_proid].totalStates +1;
+        
+                        }
 
 
 }
 
-/// Working in Progress
+
+
+//Working in progress
