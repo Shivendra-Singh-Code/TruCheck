a0e790e8de18e45842e7b1f7ef44739149d46309
Shivendra Singh <92018084+Shivendra-Singh-Code@users.noreply.github.com>
2023-04-03 15:57:06 +0530

    Update prodinfo.sol

--- contracts/prodinfo.sol
+++ contracts/prodinfo.sol
@@ -42,17 +42,44 @@ contract prodinfo{
     function create_prod(
                             uint256  _proid,
 
-                            string memory _proname
+                            string memory _proname,
+
+                            string memory _currentState,
+
+                            //uint _timestamp,
+
+                            string memory _souadd,
+
+                            string memory _desadd,
+
+                            //address _roleAddress,
+
+                            string memory _role
 
                         ) public returns ( bool) {
                                 //uint blockno=1;
-                                prod memory _prod=prod({proid: _proid, proname: _proname, totalStates: 0});
+                                prodata[_proid]=prod({proid: _proid, proname: _proname, totalStates: 0});
                                 
-                                prodata[_proid]=_prod;
+                                prod storage _prod= prodata[_proid];
+ 
+                                States memory newState = States({ currentState: _currentState,
+                                                            time_stamp: block.timestamp ,
+                                                            souadd: _souadd, 
+                                                            desadd: _desadd,
+                                                            roleAddress: msg.sender,
+                                                            role: _role   });
+                            
+                            prodata[_proid].positions[ prodata[_proid].totalStates ]=newState;
+        
+                            prodata[_proid].totalStates = prodata[_proid].totalStates +1;
+
                                 return true;
 
                                                               
                         }
+
+
+
     function updatestate(   
                         uint _proid,
 
@@ -60,7 +87,7 @@ contract prodinfo{
 
                         //uint _timestamp,
 
-                        string memory _souadd,
+                        //string memory _souadd,
 
                         string memory _desadd,
 
@@ -68,7 +95,11 @@ contract prodinfo{
 
                         string memory _role ) public returns (string memory){
 
+                            prod storage p=prodata[_proid];
                             
+                            uint count = p.totalStates -1;
+                            
+                            string memory _souadd=p.positions[count].desadd;
 
                             States memory newState = States({ currentState: _currentState,
                                                             time_stamp: block.timestamp ,
@@ -82,10 +113,37 @@ contract prodinfo{
                             prodata[_proid].totalStates = prodata[_proid].totalStates +1;
         
                         }
+    
+    function get_prodata( uint _proid) public returns ( uint , string memory, uint ){
 
+        prod storage p=prodata[_proid];
+         return (p.proid, p.proname, p.totalStates);
+        
+        //return( p.proid, p.proname);
+        /*for (uint256 j=0; j<=p.totalStates; j++){
+            return(
+                p.positions[j].currentState,
+                p.positions[j].role
+            );
+        }*/
+        
+
+    }
+
+    function get_prostates(uint _proid, 
+                           uint j ) public returns (string memory, 
+                                                        uint ,
+                                                        string memory, 
+                                                        string memory,
+                                                        address, 
+                                                        string memory){
+                                                            prod storage p=prodata[_proid];
+                                                            return(p.positions[j].currentState,
+                                                                   p.positions[j].time_stamp,
+                                                                   p.positions[j].souadd,
+                                                                   p.positions[j].desadd,
+                                                                   p.positions[j].roleAddress,
+                                                                   p.positions[j].role);
+                                                        }
 
 }
-
-
-
-//Working in progress
